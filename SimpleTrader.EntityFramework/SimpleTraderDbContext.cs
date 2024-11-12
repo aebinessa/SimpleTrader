d78af241fb47dcee5043786ea746023ea574d31e
abdullah bin essa <abinessa0004@sdsu.edu>
2024-11-12 07:08:46 +0100

    init commit

---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=sql,1433;Database=SimpleTraderDb;User Id=SA;Password=reallyStrongPwd123;"
+, sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5, // Maximum number of retries
+                    maxRetryDelay: TimeSpan.FromSeconds(30), // Delay between retries
+                    errorNumbersToAdd: null // Additional SQL error numbers to consider transient
+                );
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
